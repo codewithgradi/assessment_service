5b88eaf5ea0edf01d2ef6f260bd5cbbd1caadf93
Gradi Puata <gradipuata@gmail.com>
2026-01-18 19:11:03 +0200

    Added Validations on dto

--- Dtos/Assesment/CreateAssessmentDto.cs
+++ Dtos/Assesment/CreateAssessmentDto.cs
@@ -1,7 +1,12 @@
+
+using System.ComponentModel.DataAnnotations;
+
 public class CreateAssessmentDto
 {
   public int Id { get; set; }
+  [Required]
   public string? Name { get; set; }
+  [Required]
   public int MaxMark { get; set; }
   public List<QuestionT>? Questions { get; set; }
 }
--- Dtos/Questions/CreateQuestionDto.cs
+++ Dtos/Questions/CreateQuestionDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 public class CreateQuestionDto
 {
   public int Id { get; set; }
+  [Required]
   public string? Question { get; set; }
+  [Required]
   public string? CorrectAnswer { get; set; }
+  [Required]
   public QuestionLevel Level { get; set; }
   public List<string>? Options { get; set; }
   public int Mark { get; set; }
